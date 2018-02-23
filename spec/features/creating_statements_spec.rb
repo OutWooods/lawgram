6c955339b07ee93a54c53cb3755b2e50d167196a
Tom Grand <thomas.a.grand@gmail.com>
2018-02-23 21:05:19 +0000

    add statement feature test

---
+++ spec/features/creating_statements_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper.rb'
+
+feature 'Creating statements' do
+  scenario 'can create a statement' do
+    visit '/'
+    click_link 'New Statement'
+    attach_file('Image', 'spec/files/images/lawyer.jpg')
+    fill_in 'Evidence', with: 'I am a big clever lawyer'
+    click_button 'Create Statement'
+    expect(page).to have_content('clever lawyer')
+    expect(page).to have_css('img[src*="coffee.jpg"]')
+  end
+end
