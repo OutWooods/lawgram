e134504fabe4edbe6f4a78a2ee52f4957d1d7f6c
Tom Grand <thomas.a.grand@gmail.com>
2018-02-24 14:50:15 +0000

    test login

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 feature 'login' do
   scenario 'I can log in with a cool username' do
     visit '/users/sign_up'
-    fill_in :username, with: 'awesomeLawyer'
-    fill_in :email, with: 'awesome_lawyer@law.com'
-    fill_in :password, with: 'iamawesome'
-    fill_in :password_check, with: 'iamawesome'
+    fill_in 'User name', with: 'awesomeLawyer'
+    fill_in :Email, with: 'awesome_lawyer@law.com'
+    fill_in 'user[password]', with: 'iamawesome'
+    fill_in 'Password confirmation', with: 'iamawesome'
+    click_button 'Sign up'
     expect(page).to have_current_path('/')
-    expect(page).to have_content('Welcome awesomeLawyer!')
+    expect(page).to have_content('Welcome awesome Lawyer!')
   end
 end
