711325a3bde28dec0d50b9f061e790837c29a01c
Tom Grand <thomas.a.grand@gmail.com>
2018-02-24 15:24:35 +0000

    minimum username length

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -6,4 +6,11 @@ feature 'login' do
     expect(page).to have_current_path('/')
     expect(page).to have_content('Welcome! You have joined the greatest website ever. Good move!')
   end
+
+  scenario 'I can\'t login with a username shorter than 4' do
+    login('aaa@aa.com', 'sht')
+    expect(page).to have_current_path('/users')
+    expect(page).to_not have_content('Welcome! You have joined the greatest website ever. Good move!')
+    expect(page).to have_content('User nameis too short (minimum is 4 characters) ')
+  end
 end
