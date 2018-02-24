c007cbd6f46d8f7be1def4d221c0058e2cc0fee4
Tom Grand <thomas.a.grand@gmail.com>
2018-02-24 11:03:19 +0000

    add show links for each piece of evidence

--- spec/features/view_evidence_feed_spec.rb
+++ spec/features/view_evidence_feed_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
 feature 'evidence feed' do
+   background do
+     statement_one = create(:statement, evidence: 'This is the first')
+     statement_two = create(:statement, evidence: 'This is the second')
+     visit '/'
+   end
+
   scenario 'displays multiple statements' do
-    statement_one = create(:statement, evidence: 'This is the first')
-    statement_two = create(:statement, evidence: 'This is the second')
-    p Statement.all
-    visit '/'
     expect(page).to have_content('This is the first')
     expect(page).to have_content('This is the second')
     expect(page).to have_css('img[src*="lawyer.jpg"]')
+  end
+
+  scenario 'visit an individual statement' do
+     page.all(:link, "See evidence")[1].click
+     expect(page).not_to have_content('This is the first')
+     expect(page).to have_content('This is the second')
+     expect(page).to have_css('img[src*="lawyer.jpg"]')
   end
 end
